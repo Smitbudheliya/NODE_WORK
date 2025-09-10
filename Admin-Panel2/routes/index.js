ea5fa2cda89fda58405dd373acf98a98db4eb970
Smitbudheliya <budheliyasmit@gmail.com>
2025-09-10 19:08:33 +0530

    first commit

---
+++ Admin-Panel2/routes/index.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const { loginPage, loginUser , logoutUser, forgotPassword, sendEmailWithOtp, verifyOtp, resetPassword , resetPasswordPage} = require("../controllers/authCtr");
+const { body } = require("express-validator");
+const routes=express.Router();
+
+
+routes.get('/',loginPage)
+routes.use('/admin',require('./admin_Routes'))
+// validation ke lia
+routes.post('/login', [
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long")
+], loginUser);
+
+routes.get('/logout',logoutUser)
+routes.get('/forgotPassword',forgotPassword)
+routes.post('/send-email',sendEmailWithOtp)
+routes.post('/verify-otp',verifyOtp)
+routes.get('/verify-otp',verifyOtp)
+routes.post('/reset-password',resetPassword)
+routes.get('/resetPassword',resetPasswordPage)
+
+module.exports=routes;
